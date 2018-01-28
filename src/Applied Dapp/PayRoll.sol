237eab4f290f39f5d11e1675de79aa77d889a296
yangc8128 <harpsbro2283@gmail.com>
2018-01-28 15:11:10 -0800

    develop: Combined the three parts of the simple dapp together

--- src/Applied Dapp/PayRoll.sol
+++ src/Applied Dapp/PayRoll.sol
@@ -60,3 +60,124 @@ contract EmploymentRecord is owned {
 
 }
 
+
+contract Payment is owned {
+    event PaymentCreationEvent (
+      address owner,
+      address receiver,
+      uint payInDollars
+    );
+
+    // https://ethereum.stackexchange.com/questions/18192/how-do-you-work-with-date-and-time-on-ethereum-platform
+    event PaymentEvent (
+      address owner,
+      address receiver,
+      uint payInDollars,
+      uint datePaid // LOOK INTO FURTHER (timestamp, now)
+    );
+
+    uint constant MONTHLY = 2629743;
+    uint constant WEEKLY = 604800;
+    uint constant SEMI_MONTHLY = 1314871;
+    uint constant BI_WEEKLY = 302400;
+
+    // Contract members
+    address receiver;
+    // Representative of onetime/wage/salary pay
+    uint payInDollars;
+    uint lastUpdate;
+    bool payCondition = false;
+
+    function Payment(address _receiver, uint _pay) public {
+      receiver = _receiver;
+      payInDollars = _pay;
+      lastUpdate = now;
+
+      PaymentCreationEvent(owner,_receiver,_pay);
+    }
+
+    function setPayCondition() public;
+
+    function payout() public onlyOwner {
+        if (!payCondition) revert(); // Return gas
+        // take from owner
+        // send to owner
+
+        PaymentEvent(owner,receiver,payInDollars,now);
+    }
+
+}
+
+
+contract PermanentPay is Payment {
+    uint payFrequency;
+
+    // http://solidity.readthedocs.io/en/develop/contracts.html#arguments-for-base-constructors
+    function PermanentPay(address _receiver, uint _pay, uint _payFrequency) public Payment(_receiver,_pay) {
+      payFrequency = _payFrequency;
+    }
+
+    function setPayCondition() public {
+        payCondition = true;
+    }
+}
+
+contract CasualPay is Payment {
+    function CasualPay(address _receiver, uint _pay) public Payment(_receiver,_pay) {}
+
+    function setPayCondition() public onlyOwner {
+      payCondition = true;
+    }
+}
+
+contract ContractPay is Payment {
+    uint payFrequency;
+    uint contractEndTime;
+
+    function ContractPay(address _receiver, uint _pay, uint _payFrequency, uint _endTime) public Payment(_receiver,_pay) {
+      contractEndTime = _endTime;
+      payFrequency = _payFrequency;
+    }
+
+    function setPayCondition() public {
+        payCondition = true;
+    }
+
+    function earlyTermination() public onlyOwner {
+      close();
+    }
+}
+
+// https://ethereum.stackexchange.com/questions/27777/deploying-contract-factory-structure-in-remix
+// https://blog.aragon.one/advanced-solidity-code-deployment-techniques-dc032665f434
+contract PaymentFactory is owned {
+    enum EmploymentType {PERM, CASUAL, CONTRACT}
+
+    // index of created payment contracts
+    address[] public paymentContracts;
+
+    function getPaymentContractCount() public constant returns(uint _length) {
+        return paymentContracts.length;
+    }
+
+    // function pointer equivalent: https://ethereum.stackexchange.com/questions/3342/pass-a-function-as-a-parameter-in-solidity
+    // https://ethereumdev.io/manage-several-contracts-with-factories/
+    function createPayment(EmploymentType _status) public onlyOwner returns(address _newPayment) {
+        if (_status == PaymentFactory.EmploymentType.PERM) {
+            PermanentPay _perm = new PermanentPay(_employee,100,100);
+            paymentContracts.push(_perm);
+            return _perm;
+        } else if (_status == PaymentFactory.EmploymentType.PERM) {
+            CasualPay _casual = new CasualPay(_employee,100);
+            paymentContracts.push(_casual);
+            return _casual;
+        } else if (_status == PaymentFactory.EmploymentType.PERM) {
+            ContractPay _contract = new ContractPay(_employee,100,100,100);
+            paymentContracts.push(_contract);
+            return _contract;
+        } else {
+            revert();
+        }
+    }
+}
+
