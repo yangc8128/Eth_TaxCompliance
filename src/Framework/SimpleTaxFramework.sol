bef107f019451bad48acd9704db5b34d14a2a68d
yangc8128 <harpsbro2283@gmail.com>
2018-02-01 00:05:52 -0800

    SimpleDapp_GUI: Changed button names to better differentiate with other buttons. Altered code in index.html, to match Payroll than Coursetro's example. Added comments to note concern for TaxFramework

--- src/Framework/SimpleTaxFramework.sol
+++ src/Framework/SimpleTaxFramework.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.4.16;
 
+/*
+    First Concern:
+        Classification
+        Map employment classification to Tax classification
+        Classification
+          - Permanent
+          - Fulltime
+          - Contract
+          - Independent
+          - Dependent
+          - Tax Exempt Employee
+          - Non-Tax Exempt Employee
+        Many to few
+    Second Concern:
+        Commit the withholding
+
+*/
 contract owned {
     function owned() public { owner = msg.sender; }
     address owner;
