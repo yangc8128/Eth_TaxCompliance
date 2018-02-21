0adb05a41a3925a7d2278a8f4e93d2990dac770b
yangc8128 <harpsbro2283@gmail.com>
2018-02-20 21:30:25 -0800

    Framework_dev: Overhaul the codebase to match more modular pieces

--- src/Framework/SimpleTaxFramework.sol
+++ src/Framework/SimpleTaxFramework.sol
@@ -1,22 +1,5 @@
 pragma solidity ^0.4.16;
 
-/*
-    First Concern:
-        Classification
-        Map employment classification to Tax classification
-        Classification
-          - Permanent
-          - Fulltime
-          - Contract
-          - Independent
-          - Dependent
-          - Tax Exempt Employee
-          - Non-Tax Exempt Employee
-        Many to few
-    Second Concern:
-        Commit the withholding
-
-*/
 contract Owned {
     function Owned() public { owner = msg.sender; }
     address owner;
