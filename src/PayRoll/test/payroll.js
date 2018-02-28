643b3683179c4ee84a5877d9e545cae32231b054
yangc8128 <harpsbro2283@gmail.com>
2018-02-28 15:46:41 -0800

    Framework_dev: Added timemachine code from Medium article. Added additional questions to notes in Truffle test script.

--- src/PayRoll/test/payroll.js
+++ src/PayRoll/test/payroll.js
@@ -1,21 +1,64 @@
 /*
- * Consider: return meta.sendCoin(account_two, amount, {from: account_one});
- * From MetaCoin.sol the method is sendCoind(address, uint)
- * The added parameter in the list has modified the address, i.e. msg.sender
- */
-
-/*
  * Test Environment Questions:
+ * How do you change the caller of a function to simulate real world use cases? [1]
+ *    Consider: return meta.sendCoin(account_two, amount, {from: account_one});
+ *    From MetaCoin.sol the method is sendCoind(address, uint)
+ *    The added parameter in the list has modified the address, i.e. msg.sender
+ * 
  * When deploying contracts at the beginning with artifacts, is it accounts[0], that does it?
  * How do I deploy a new contract in the contract_js test?
- * Do asynchronous tests run in parallel grouped via "it()"?
- * How do I simulate time passing?
- * How do I initialize a Payment with appropriate endTimes?
+ * Do asynchronous tests run in parallel grouped via "it()"? [2]
+ *    Works off of the pair of await and a promise function such as ___.cal()
+ *    Error with async, needs to be wrapped with a try/catch
+ * 
+ * How do I simulate time passing? [2]
+ *    ____use evm_increaseTime____
+ *    // Example for preparing time manipulation:
+      const timeTravel = function (time) {
+        return new Promise((resolve, reject) => {
+          web3.currentProvider.sendAsync({
+            jsonrpc: "2.0",
+            method: "evm_increaseTime",
+            params: [time], // 86400 is num seconds in day
+            id: new Date().getTime()
+          }, (err, result) => {
+            if(err){ return reject(err) }
+            return resolve(result)
+          });
+        })
+      }
+ *    // Example for using time manipulation:
+      it("should successfully call specialFn because enough time passed", async function () {
+        let meta = await MetaCoin.new();
+        await timeTravel(86400 * 3) //3 days later
+        await mineBlock() // workaround for https://github.com/ethereumjs/testrpc/issues/336
+        let status = await meta.specialFn.call();
+        assert.equal(status, true, "specialFn should be callable after 1 day")
+      })
+ *
+ * How do I initialize a Payment with appropriate endTimes? [2]
+ *    utilize number in uints of seconds for time (86400 secs = 1 days)
+ *
  * How do I initialize a Payment with appropriate ether to $, without fiat transactions?
  */
 
 // Used for testing in application context
 var PayRoll = artifacts.require("./PayRoll.sol");
+
+// Helper Test Function
+const timeTravel = function (time) {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: "2.0",
+      method: "evm_increaseTime",
+      params: [time], // 86400 is num seconds in day
+      id: new Date().getTime()
+    }, (err, result) => {
+      if(err){ return reject(err) }
+      return resolve(result)
+    });
+  })
+}
 
 contract('PayRoll', function(accounts) {
   it("should determine owner of PayRoll", function() {
@@ -62,6 +105,7 @@ contract('PayRoll', function(accounts) {
 
 
   /*
+  // Requires timemachine
   // Attempt to timely ask for a payout on a Payment from employee
   it("should pay employee from Payment", function() {
     return PayRoll.deployed().then(function(instance) {
@@ -77,4 +121,5 @@ contract('PayRoll', function(accounts) {
   */
 }); // End of contract
 
-// Truffle JS Test Documentation: http://truffleframework.com/docs/getting_started/javascript-tests
+// [1] Truffle JS Test Documentation: http://truffleframework.com/docs/getting_started/javascript-tests
+// [2] Medium Article on Truffle async / await: https://medium.com/@angellopozo/testing-solidity-with-truffle-and-async-await-396e81c54f93
